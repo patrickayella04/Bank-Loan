38841dcb71e7488ab843cab7d0a3f7d93e6eddae
patrickayella04 <60228005+patrickayella04@users.noreply.github.com>
2020-07-28 23:54:28 +0100

    revision of code

--- app.js
+++ app.js
@@ -8,35 +8,53 @@ function calculateResults(e) {
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
 
+    const monthlyPayments = document.getElementById('monthly-payments');
+    const totalPayments = document.getElementById('total-payments');
+    const totalInterest = document.getElementById('total-interest');
+
     const p = amount.value;
     const n = years.value;
     const r = (interest.value) / 100 / 12;
 
     const x = Math.pow((1 + r), (n * 12));
 
+    
+
     const monthlyPayments = (p * (r * x)) / (x - 1);
-
-
-    document.getElementById('monthly-payments').value = monthlyPayments;
-
-
-    const totalPayments = (monthlyPayments * (n * 12));
-
-    document.getElementById('total-payments').value = totalPayments;
-
-
-    const totalInterest = totalPayments - p;
-
-
-    document.getElementById('total-interest').value = totalInterest;
-
-
-    console.log(totalInterest);
-
 
 
     
 
 
+    const totalPayments = (monthlyPayments * (n * 12));
+
+   
+
+
+    const totalInterest = totalPayments - p;
+
+
+    
+
+
+    console.log(totalInterest);
+
+
+    if (isFinite) {
+    monthlyPayments.value
+    totalPayments.value
+    totalInterest.value
+        
+    } else {
+        // error message
+        setTimeout(Showerror(error), 3000);
+    }
+
+
     e.preventDefault();
 };
+
+
+function Showerror() {
+    console.log('Please check your numbers...');
+}
